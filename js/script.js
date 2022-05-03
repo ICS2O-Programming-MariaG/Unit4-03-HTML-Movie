da4f0e06530a8fca55452329a0593db728a89c3e
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-05-03 14:32:14 +0000

    Added constants for age thresholds

--- js/script.js
+++ js/script.js
@@ -10,17 +10,21 @@ function ageSent() {
   //initializing and declaring variables
   let age = parseInt(document.getElementById("user-age").value);
   let message = "";
+  //Setting constants: first age threshold is 17, second is 13, third is 5
+  const FIRST_THRESHOLD = 17;
+  const SECOND_THRESHOLD = 13;
+  const THIRD_THRESHOLD = 5;
   //beginning if statement: if user age is greater than or equal to 17
-  if (age >= 17) {
+  if (age >= FIRST_THRESHOLD) {
     message = "You are old enough to see an R-rated movie on your own.";
   }
   //otherwise, if age is greater than or equal to 13
-  else if (age >= 13) {
+  else if (age >= SECOND_THRESHOLD) {
     message = "You can watch a PG-13 movie alone.";
   }
   //otherwise, if age is greater than or equal to 5
-  else if (age >= 5) {
-    message = "You can see a G or PG movie on your own."
+  else if (age >= THIRD_THRESHOLD) {
+    message = "You can see a G or PG movie on your own.";
   }
   //else (any age less than 5)
   else {
